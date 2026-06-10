2e726078c03f32acd8aef15d00c5936642941a01
DuyViet04 <duyviet130904@gmail.com>
2026-06-10 19:58:16 +0700

    update

---
+++ Assets/MatchFactoryCore/Scripts/Data/DataLevelMatchFactoryNew.cs
@@ -0,0 +1,16 @@
+﻿using MatchFactoryCore.Scripts.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assets.MatchFactoryCore.Scripts.Data
+{
+    [Serializable]
+    public class DataLevelMatchFactoryNew
+    {
+        public int Level;
+        public float TimeOnLevel;
+        public Dictionary<ItemFactoryType, int> TargetItemDictionary;
+        public Dictionary<ItemFactoryType, int> OtherItemDictionary;
+    }
+}
